59f9770edafc7a57d4f943aab3a7e53231ef5c40
arifsptra <arif.sptrra@gmail.com>
2023-07-07 11:32:57 +0700

    make logic to save user and member, and make basic response

--- src/main/java/com/mycompany/chatbot/Chatbot.java
+++ src/main/java/com/mycompany/chatbot/Chatbot.java
@@ -4,8 +4,17 @@
 
 package com.mycompany.chatbot;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotOptions;
 
 /**
  *
@@ -13,21 +22,156 @@ import org.telegram.telegrambots.meta.api.objects.Update;
  */
 public class Chatbot extends TelegramLongPollingBot {
 
+    // Telegram Bot Credentials
     private static final String BOT_TOKEN = "6395733810:AAHocLQFZH_aFhjUhg2XZOZ1X4Zvf-85JlQ";
+    private static final String BOT_USERNAME = "arifsaputra_bot";
+    
+    // Database Credentials
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/db_chatbot";
+    private static final String DB_USERNAME = "arif";
+    private static final String DB_PASSWORD = "Koentj1@$";
+    
+    // Flag to track registration status
+    private static final String REGISTERED_FLAG = "REGISTERED";
 
     @Override
     public void onUpdateReceived(Update update) {
-        String message=update.getMessage().getText();
-        System.out.println(message);
+        if(update.hasMessage()){
+            Message message = update.getMessage();
+            Long chatId = message.getChatId();
+            String sChatId = Long.toString(chatId);
+            String text = message.getText();
+            String username = message.getFrom().getUserName();
+
+            System.out.println(sChatId);
+            System.out.println(text);
+            System.out.println(username);
+
+            saveUserData(sChatId, username);
+//            
+//            if(text.equals("/daftar")) {
+//                saveMemberData(sChatId, username);
+//                sendResponse(chatId, "Terima kasih telah mendaftar!");
+//            }
+//            
+//            if(text.equals("/cuaca")) {
+//                sendResponse(chatId, "Cuaca Hari ini Dingin!");
+//            }
+             // Check if the user is registered
+             
+            boolean isRegistered = isUserRegistered(sChatId);
+            if (!isRegistered) {
+                if (text.equals("/daftar")) {
+                    saveMemberData(sChatId, username);
+                    sendResponse(chatId, "Terima kasih telah mendaftar!");
+                } else {
+                    sendResponse(chatId, "Silakan daftar terlebih dahulu dengan menggunakan perintah /daftar");
+                }
+            } else {
+                if (text.equals("/cuaca")) {
+                    sendResponse(chatId, "Cuaca Hari ini Dingin!");
+                } else {
+                    sendResponse(chatId, "Pesan lain yang diterima setelah pendaftaran.");
+                }
+            }
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return "arifsaputra_bot";
+        return BOT_USERNAME;
     }
 
     @Override
     public String getBotToken() {
         return BOT_TOKEN;
+    }
+    
+    private boolean isUserRegistered(String chatId) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String sql = "SELECT * FROM member WHERE chat_id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, chatId);
+            ResultSet resultSet = statement.executeQuery();
+
+            boolean isRegistered = resultSet.next();
+
+            resultSet.close();
+            statement.close();
+
+            return isRegistered;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    private void saveUserData(String chatId, String username) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String sql = "INSERT INTO user (chat_id, username) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, String.valueOf(chatId)); // Convert chatId to String
+            statement.setString(2, username);
+
+            // Check if the user already exists in the database
+            String checkUserExistsSql = "SELECT * FROM user WHERE chat_id = ?";
+            PreparedStatement checkUserExistsStatement = connection.prepareStatement(checkUserExistsSql);
+            checkUserExistsStatement.setString(1, String.valueOf(chatId)); // Convert chatId to String
+            ResultSet resultSet = checkUserExistsStatement.executeQuery();
+
+            if (resultSet.next()) {
+                System.out.println("User already exists in the database.");
+            } else {
+                // User does not exist, save the new user data
+                statement.executeUpdate();
+                System.out.println("New user data saved to the database.");
+            }
+
+            resultSet.close();
+            checkUserExistsStatement.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void saveMemberData(String chatId, String username) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String sql = "INSERT INTO member (chat_id, username) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, String.valueOf(chatId)); // Convert chatId to String
+            statement.setString(2, username);
+
+            // Check if the user already exists in the database
+            String checkUserExistsSql = "SELECT * FROM member WHERE chat_id = ?";
+            PreparedStatement checkUserExistsStatement = connection.prepareStatement(checkUserExistsSql);
+            checkUserExistsStatement.setString(1, String.valueOf(chatId)); // Convert chatId to String
+            ResultSet resultSet = checkUserExistsStatement.executeQuery();
+
+            if (resultSet.next()) {
+                System.out.println("Member already exists in the database.");
+            } else {
+                // User does not exist, save the new user data
+                statement.executeUpdate();
+                System.out.println("New member data saved to the database.");
+            }
+
+            resultSet.close();
+            checkUserExistsStatement.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendResponse(Long chatId, String response) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(response);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
