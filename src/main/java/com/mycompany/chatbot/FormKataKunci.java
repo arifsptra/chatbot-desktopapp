34cc3822e461201c388a05471f13a97ca526e546
arifsptra <arif.sptrra@gmail.com>
2023-07-08 21:39:07 +0700

    make logic to manage keyword

--- src/main/java/com/mycompany/chatbot/FormKataKunci.java
+++ src/main/java/com/mycompany/chatbot/FormKataKunci.java
@@ -4,17 +4,49 @@
  */
 package com.mycompany.chatbot;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author arif
  */
 public class FormKataKunci extends javax.swing.JFrame {
+    
+    // Database Credentials
+    public String DB_URL = "jdbc:mysql://localhost:3306/db_chatbot";
+    public String DB_USERNAME = "arif";
+    public String DB_PASSWORD = "Koentj1@$";
+    ResultSet rsBrg;
+    Statement stm;
+    private Object[][] dataTable = null;
+    private String[] header = {"Kata", "Pesan"};
 
     /**
      * Creates new form FormKataKunci
      */
     public FormKataKunci() {
         initComponents();
+        
+        bacaData();
+        
+        // mouse click tabel barang
+        tabelKataKunci.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                setField();
+            }
+        });
     }
 
     /**
@@ -26,22 +58,287 @@ public class FormKataKunci extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        txtKata = new javax.swing.JTextField();
+        txtPesan = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabelKataKunci = new javax.swing.JTable();
+        btnDaftar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        btnClear = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        btnHapus = new javax.swing.JButton();
+        btnEdit = new javax.swing.JButton();
+        btnKeluar = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        btnRefresh = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        txtKata.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        txtKata.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtKataActionPerformed(evt);
+            }
+        });
+
+        txtPesan.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+
+        jLabel5.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        jLabel5.setText("Pesan");
+
+        tabelKataKunci.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        tabelKataKunci.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Kata", "Aksi"
+            }
+        ));
+        jScrollPane1.setViewportView(tabelKataKunci);
+
+        btnDaftar.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        btnDaftar.setText("Daftar");
+        btnDaftar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDaftarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Poppins Medium", 0, 18)); // NOI18N
+        jLabel1.setText("Kelola Kata Kunci");
+
+        btnClear.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        btnClear.setText("Clear");
+        btnClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Poppins Medium", 0, 18)); // NOI18N
+        jLabel2.setText("Data Kata Kunci");
+
+        btnHapus.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        btnHapus.setText("Hapus");
+        btnHapus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHapusActionPerformed(evt);
+            }
+        });
+
+        btnEdit.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        btnEdit.setText("Edit");
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
+
+        btnKeluar.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        btnKeluar.setText("Keluar");
+        btnKeluar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKeluarActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        jLabel4.setText("Kata");
+
+        btnRefresh.setFont(new java.awt.Font("Poppins Medium", 0, 15)); // NOI18N
+        btnRefresh.setText("Refresh");
+        btnRefresh.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRefreshActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(txtKata, javax.swing.GroupLayout.PREFERRED_SIZE, 244, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(txtPesan, javax.swing.GroupLayout.PREFERRED_SIZE, 244, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnDaftar, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnClear, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnHapus, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnRefresh)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnKeluar)))
+                .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(txtKata, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(txtPesan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 42, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnDaftar)
+                    .addComponent(btnClear)
+                    .addComponent(btnHapus)
+                    .addComponent(btnEdit)
+                    .addComponent(btnKeluar)
+                    .addComponent(btnRefresh))
+                .addGap(20, 20, 20))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnDaftarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDaftarActionPerformed
+        // TODO add your handling code here:
+        String tKata = txtKata.getText();
+        String tPesan = txtPesan.getText();
+        if(!tKata.isEmpty() && !tPesan.isEmpty()) {
+            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+                String sql = "INSERT INTO kunci (kata, pesan) VALUES (?, ?)";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, tKata);
+                statement.setString(2, tPesan);
+                statement.executeUpdate();
+                statement.close();
+
+                // Refresh tabel data
+                bacaData();
+            } catch (SQLException exception) {
+                JOptionPane.showMessageDialog(null, exception);
+            }
+        }
+    }//GEN-LAST:event_btnDaftarActionPerformed
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+        // TODO add your handling code here:
+        String tKata = txtKata.getText();
+        String tPesan = txtPesan.getText();
+        if(!tKata.isEmpty() && !tPesan.isEmpty()) {
+            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+                String sql = "UPDATE kunci SET pesan=? WHERE kata=?";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, tPesan);
+                statement.setString(2, tKata);
+                statement.executeUpdate();
+                statement.close();
+
+                // Refresh tabel data
+                bacaData();
+            } catch (SQLException exception) {
+                JOptionPane.showMessageDialog(null, exception);
+            }
+        }
+    }//GEN-LAST:event_btnEditActionPerformed
+
+    private void btnKeluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKeluarActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_btnKeluarActionPerformed
+
+    private void txtKataActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtKataActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtKataActionPerformed
+
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
+        // TODO add your handling code here:
+        txtKata.setText("");
+        txtPesan.setText("");
+    }//GEN-LAST:event_btnClearActionPerformed
+
+    private void btnRefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRefreshActionPerformed
+        // TODO add your handling code here:
+        // Refresh tabel data
+        bacaData();
+    }//GEN-LAST:event_btnRefreshActionPerformed
+
+    private void btnHapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHapusActionPerformed
+        // TODO add your handling code here:
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String sql="delete from kunci where kata='" + txtKata.getText() + "'";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.executeUpdate(sql);
+            
+            // Refresh tabel data
+            bacaData();
+        }
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }//GEN-LAST:event_btnHapusActionPerformed
+
+    // method set field form jika salah satu field tabel ditekan
+    private void setField()
+    {
+        int row=tabelKataKunci.getSelectedRow();
+        txtKata.setText((String)tabelKataKunci.getValueAt(row,0));
+        txtPesan.setText((String)tabelKataKunci.getValueAt(row,1));
+    }
+
+    // method baca data dari Mysql dimasukkan ke table pada form
+    private void bacaData() {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            stm = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rsBrg = stm.executeQuery("select * from kunci");
+            ResultSetMetaData meta = rsBrg.getMetaData();
+            int col = meta.getColumnCount();
+            int baris = 0;
+            while(rsBrg.next()) {
+                baris = rsBrg.getRow();
+            }
+            dataTable = new Object[baris][col];
+            int x = 0;
+            rsBrg.beforeFirst();
+            while(rsBrg.next()) {
+                dataTable[x][0] = rsBrg.getString("kata");
+                dataTable[x][1] = rsBrg.getString("pesan");
+                x++;
+            }
+            tabelKataKunci.setModel(new DefaultTableModel(dataTable,header));
+        }
+        catch(SQLException e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -78,5 +375,19 @@ public class FormKataKunci extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClear;
+    private javax.swing.JButton btnDaftar;
+    private javax.swing.JButton btnEdit;
+    private javax.swing.JButton btnHapus;
+    private javax.swing.JButton btnKeluar;
+    private javax.swing.JButton btnRefresh;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tabelKataKunci;
+    private javax.swing.JTextField txtKata;
+    private javax.swing.JTextField txtPesan;
     // End of variables declaration//GEN-END:variables
 }
